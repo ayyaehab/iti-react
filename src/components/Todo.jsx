67172a5cd6b00f16301244e6855865ea42f8ef3e
ayyaehab <ayyaehab66@gmail.com>
2021-10-21 15:50:05 +0200

    lab2

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,11 @@
-export const Todo = (props) => {
-return (
+// export const Todo = (props) => {
+// return (
 
-    <div>
-        <h3>{props.title}</h3>
-        <p>{props.details}</p>
-    </div>
-);
+//     <div>
+//         <h3>{props.title}</h3>
+//         <p>{props.details}</p>
+//     </div>
+// );
 
 
-};
+// };
